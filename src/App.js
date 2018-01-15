ff25c1cf72c8d763da777cec0e79fb95227baded
Michael Gauthier <mike@silverorange.com>
2018-01-15 18:33:19 -0400

    Remove SVG logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PullRequestChart from './PullRequestChart';
 import SiteReleasesChart from './SiteReleasesChart';
