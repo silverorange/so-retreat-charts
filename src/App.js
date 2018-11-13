1656ea1e8935bbda49fcd3c74cd7288dcd1aa606
Michael Gauthier <mike@silverorange.com>
2018-11-13 17:24:10 -0400

    Extract all data into a separate file

--- src/App.js
+++ src/App.js
@@ -1,99 +1,19 @@
 import React from 'react';
 import Chart from './Chart';
+import data from './data';
 
 import './App.css';
 
-const pullRequests = [
-  {
-    year: 2014,
-    value: 2029
-  },
-  {
-    year: 2015,
-    value: 2209
-  },
-  {
-    year: 2016,
-    value: 2418
-  },
-  {
-    year: 2017,
-    value: 3375
-  }
-];
-
-const siteReleases = [
-  {
-    year: 2015,
-    value: 244
-  },
-  {
-    year: 2016,
-    value: 516
-  },
-  {
-    year: 2017,
-    value: 753
-  }
-];
-
-const repositories = [
-  {
-    year: 2015,
-    value: 70
-  },
-  {
-    year: 2016,
-    value: 103
-  },
-  {
-    year: 2017,
-    value: 128
-  }
-];
-
-const iOSReleases = [
-  {
-    year: 2014,
-    value: 3
-  },
-  {
-    year: 2015,
-    value: 23
-  },
-  {
-    year: 2016,
-    value: 19
-  },
-  {
-    year: 2017,
-    value: 7
-  }
-];
-
-const androidReleases = [
-  {
-    year: 2015,
-    value: 25
-  },
-  {
-    year: 2016,
-    value: 16
-  },
-  {
-    year: 2017,
-    value: 7
-  }
-];
+const dataSet = 'platforms2018';
 
 export default function App() {
   return (
     <div className="App">
-      <Chart yTicks={8} yDomain={[0, 3500]} data={pullRequests} />
-      <Chart yTicks={7} yDomain={[0, 800]} data={siteReleases} />
-      <Chart yTicks={7} yDomain={[0, 140]} data={repositories} />
-      <Chart yTicks={5} yDomain={[0, 30]} data={iOSReleases} />
-      <Chart yTicks={5} yDomain={[0, 30]} data={androidReleases} />
+      <Chart
+        yTicks={data[dataSet].ticks}
+        yDomain={data[dataSet].domain}
+        data={data[dataSet].data}
+      />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,253 @@
+// 29 CFSC API deploys to EB
+// 91 deploys by Noel and Robert
+
+export default {
+  pullRequests: {
+    domain: [0, 3500],
+    ticks: 8,
+    data: [
+      {
+        year: 2014,
+        value: 2029
+      },
+      {
+        year: 2015,
+        value: 2209
+      },
+      {
+        year: 2016,
+        value: 2418
+      },
+      {
+        year: 2017,
+        value: 3375
+      },
+      {
+        year: '2018—',
+        value: 2797
+      }
+    ]
+  },
+  siteReleases: {
+    domain: [0, 800],
+    ticks: 8,
+    data: [
+      {
+        year: 2015,
+        value: 244
+      },
+      {
+        year: 2016,
+        value: 516
+      },
+      {
+        year: 2017,
+        value: 753
+      },
+      {
+        year: '2018—',
+        value: 659
+      }
+    ]
+  },
+  repositories: {
+    domain: [0, 160],
+    ticks: 8,
+    data: [
+      {
+        year: 2015,
+        value: 70
+      },
+      {
+        year: 2016,
+        value: 103
+      },
+      {
+        year: 2017,
+        value: 128
+      },
+      {
+        year: '2018—',
+        value: 154
+      }
+    ]
+  },
+  iOSReleases: {
+    domain: [0, 30],
+    ticks: 6,
+    data: [
+      {
+        year: 2014,
+        value: 3
+      },
+      {
+        year: 2015,
+        value: 23
+      },
+      {
+        year: 2016,
+        value: 19
+      },
+      {
+        year: 2017,
+        value: 7
+      },
+      {
+        year: '2018—',
+        value: 10
+      }
+    ]
+  },
+  androidReleases: {
+    domain: [0, 30],
+    ticks: 6,
+    data: [
+      {
+        year: 2015,
+        value: 25
+      },
+      {
+        year: 2016,
+        value: 16
+      },
+      {
+        year: 2017,
+        value: 7
+      },
+      {
+        year: '2018—',
+        value: 5
+      }
+    ]
+  },
+  showAndTells2017: {
+    domain: [0, 25],
+    ticks: 5,
+    data: [
+      {
+        year: 'February',
+        value: 9
+      },
+      {
+        year: 'March',
+        value: 14
+      },
+      {
+        year: 'April',
+        value: 4
+      },
+      {
+        year: 'May',
+        value: 9
+      },
+      {
+        year: 'June',
+        value: 9
+      },
+      {
+        year: 'July',
+        value: 3
+      },
+      {
+        year: 'August',
+        value: 5
+      },
+      {
+        year: 'September',
+        value: 21
+      },
+      {
+        year: 'October',
+        value: 12
+      },
+      {
+        year: 'November',
+        value: 14
+      },
+      {
+        year: 'December',
+        value: 8
+      },
+      {
+        year: 'January',
+        value: 11
+      }
+    ]
+  },
+  platforms2017: {
+    domain: [0, 14],
+    ticks: 7,
+    data: [
+      {
+        year: 'Legacy PHP',
+        value: 11
+      },
+      {
+        year: 'Node',
+        value: 9
+      },
+      {
+        year: 'React',
+        value: 6
+      },
+      {
+        year: 'iOS',
+        value: 4
+      },
+      {
+        year: 'Android',
+        value: 4
+      },
+      {
+        year: 'Ember',
+        value: 1
+      },
+      {
+        year: 'Laravel',
+        value: 1
+      },
+      {
+        year: 'Other',
+        value: 2
+      }
+    ]
+  },
+  platforms2018: {
+    domain: [0, 14],
+    ticks: 7,
+    data: [
+      {
+        year: 'Legacy PHP',
+        value: 13
+      },
+      {
+        year: 'Node',
+        value: 13
+      },
+      {
+        year: 'React',
+        value: 13
+      },
+      {
+        year: 'iOS',
+        value: 6
+      },
+      {
+        year: 'Android',
+        value: 6
+      },
+      {
+        year: 'Ember',
+        value: 1
+      },
+      {
+        year: 'Laravel',
+        value: 1
+      },
+      {
+        year: 'Other',
+        value: 2
+      }
+    ]
+  }
+};
